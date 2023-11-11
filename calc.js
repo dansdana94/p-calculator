0ea8c0299be72753d29882eea4b5a25949a1f53d
dans <dansdana94@gmail.com>
2023-11-11 17:21:46 +0100

    calc() created to make the calculator work, and some ui improvements

--- calc.js
+++ calc.js
@@ -1,23 +1,35 @@
 let nums = document.querySelectorAll('.nums')
 let input = document.querySelector('#input')
 let clearbtn = document.querySelector('#clear')
+let equalsBtn = document.querySelector('#equalsBtn')
 let op1 = ''
 let op2 = ''
 let ope = ''
 
 function operation(op1,ope,op2){
+    op1 = parseFloat(op1);
+    op2 = parseFloat(op2);
+
     if(ope === '+') {
-        return(op1 + op2)
+        input.value = (op1 + op2)
     }
     if(ope === '*') {
-        return(op1 * op2)
+        input.value =(op1 * op2)
     }
     if(ope === '-') {
-        return(op1 - op2)
+        input.value = (op1 - op2)
     }
     if(ope === '/') {
-        return(op1 / op2)
+        input.value(op1 / op2)
     }
+}
+
+function display(){
+    nums.forEach(num =>{
+        num.addEventListener('click',()=>{
+            input.value += num.id
+        })
+    })
 }
 
 function storeVal(){
@@ -37,18 +49,17 @@ function storeVal(){
     })
 }
 
-
-function display(){
-    nums.forEach(num =>{
-        num.addEventListener('click',()=>{
-            input.value += num.id
-        })
-    })
+function calc(){
+  display()
+  storeVal()
+  let result = equalsBtn.addEventListener('click',()=>{
+        operation(op1,ope,op2)
+ })
 }
+
 
 clearbtn.addEventListener('click', (c)=>{
         c = location.reload()
 })
 
-
-storeVal()
+calc()
