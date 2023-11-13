95380bbaab9658b637965fea99cfe53be0c15ecf
dans <dansdana94@gmail.com>
2023-11-13 07:57:47 +0100

    completely refactored the calc.js file adding more functions and improving readability

--- test/cc.js
+++ test/cc.js
@@ -1,60 +1,97 @@
-let nums = document.querySelectorAll('.nums');
-let input = document.querySelector('#input');
-let clearBtn = document.querySelector('#clear');
-let equalsBtn = document.querySelector('#equals');
-let op1 = '';
-let op2 = '';
-let operator = '';
+let nums = document.querySelectorAll('.nums')
+let input = document.querySelector('#input')
+let clearbtn = document.querySelector('#clear')
+let oper = document.querySelectorAll('.oper')
+let equalsBtn = document.querySelector('#equalsBtn')
+let op1 = ''
+let op2 = ''
+let ope = ''
 
-function operate(op1, operator, op2) {
-  op1 = parseFloat(op1);
-  op2 = parseFloat(op2);
-
-  if (operator === '+') {
-    return op1 + op2;
-  }
-  if (operator === '*') {
-    return op1 * op2;
-  }
-  if (operator === '-') {
-    return op1 - op2;
-  }
-  if (operator === '/') {
-    if (op2 === 0) {
-      return 'Error';
-    }
-    return op1 / op2;
-  }
-}
-
-function display() {
-  nums.forEach(num => {
-    num.addEventListener('click', () => {
-      if (operator === '') {
-        op1 += num.id;
-      } else {
-        op2 += num.id;
+/* function to perform a mathematical calculation */
+function operate(op1,ope,op2){
+  op1 = parseFloat(op1)
+  op2 = parseFloat(op2)
+      switch(ope){
+        case '+': return(op1 + op2)
+        break;
+        case '-': return(op1 - op2)
+        break;
+        case '*': return(op1 * op2)
+        break;
+        case '/': return(op1 / op2)
+        break;
       }
-      input.value += num.id;
-    });
-  });
 }
 
-clearBtn.addEventListener('click', () => {
-  input.value = '';
-  op1 = '';
-  op2 = '';
-  operator = '';
-});
+/* functio to display content on the input based on user selection */
+function display(){
+  nums.forEach(no =>{
+    no.addEventListener('click',()=>{
+    return input.textContent +=(`${no.id}`)
+    })
+  })
+ /* oper.forEach(op =>{
+    op.addEventListener('click',()=>{
+      return input.textContent +=(`${op.id}`)
+    })
+  }) */
+}
 
-equalsBtn.addEventListener('click', () => {
-  if (op1 !== '' && op2 !== '' && operator !== '') {
-    const result = operate(op1, operator, op2);
-    input.value = result;
-    op1 = result.toString();
-    op2 = '';
-    operator = '';
-  }
-});
+/* function to store input in variable */
+function storeVal(){
+  let d = ''
+  nums.forEach(no =>{
+    no.addEventListener('click',()=>{
+      d = no.id; console.log(d)
+        if(d == '+' || d == '-' || d == '*' || d == '/'){
+           ope = d
+        }
+        else if(ope != ''){op2 += d}
+        else{op1 += d}
+        console.log(op1)
+        console.log(ope)
+        console.log(op2)
+      
+    })
+  })
+}
 
-display();
+/* alt function to store value using screen input */
+function storeVal2(){
+  display()
+  let n = '';
+  let o = '';
+  
+   oper.forEach(op =>{
+    op.addEventListener('click', ()=>{
+          n = input.textContent; input.textContent = ''; 
+          o = op.id; console.log(n);  ope = o;
+        if(op1 == ''){
+          op1 = n
+        } else if(op1 != ''){op2 = n}
+       
+       
+    })
+   })
+}
+
+/* function to run the calculator */
+function calc(){
+  storeVal2()
+  let result = equalsBtn.addEventListener('click',()=>{
+        op2 = input.textContent;
+        input.textContent=(`${operate(op1,ope,op2)}`)
+        console.log(op1)
+        console.log(ope)
+        console.log(op2)
+  })
+}
+
+clearbtn.addEventListener('click',(r)=>{
+  r = location.reload();
+  ope = '';
+  op1 = ''
+  op2 = ''
+})
+
+calc()
